c7ac9a216ad8237f88ac0e0de1e1e20866ac5108
P-Sushanth <popurisushanth@gmail.com>
2025-08-21 21:18:34 +0530

    Fix: Correct app rendering and deployment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Add this import
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Wrap your app with BrowserRouter */}
+    <BrowserRouter basename="/Typing-Test/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
